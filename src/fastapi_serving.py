f90ec1243eb880b383844a761da6d4065c871ef8
Hanyu Wu <whanyu1212@hotmail.com>
2024-01-21 01:23:50 +0800

    refactoring

--- src/fastapi_serving.py
+++ src/fastapi_serving.py
@@ -19,41 +19,42 @@ MODEL_VERSION = os.getenv("MODEL_VERSION")
 CONFIG_FILE_PATH = "./cfg/catalog.yaml"
 config = parse_cfg(CONFIG_FILE_PATH)
 
-app = FastAPI()
+
+class Model:
+    def __init__(self):
+        self.trained_model = None
+        self.important_features = None
+
+    def load_model(self):
+        try:
+            self.trained_model = mlflow.pyfunc.load_model(
+                model_uri=f"models:/{MODEL_NAME}/{MODEL_VERSION}"
+            )
+        except Exception as e:
+            logger.error(f"Failed to load model: {e}")
+            raise
+
+    def load_features_to_keep(self, config: dict):
+        self.important_features = config["reduced_features"]
+
+    def make_prediction(self, data: List[Dict[str, Any]]) -> List:
+        data_df = pd.DataFrame(data)
+        return self.trained_model.predict(data_df).tolist()
 
 
-def load_features_to_keep(config: dict) -> List[str]:
-    """
-    Load the list of features to keep from the config file.
-
-    Args:
-        config_file (str): path to the config file
-
-    Returns:
-        List[str]: a list of features to keep
-    """
-
-    return config["reduced_features"]
-
-
-# Assume this is your model loading function
-def load_model():
-    model = mlflow.pyfunc.load_model(model_uri=f"models:/{MODEL_NAME}/{MODEL_VERSION}")
-    return model
+model = Model()
 
 
 @app.router.on_startup.append
 async def startup_event():
-    global trained_model
-    global important_features
     try:
-        trained_model = load_model()
-        important_features = load_features_to_keep(config)
-        logger.info(important_features)
+        model.load_model()
+        model.load_features_to_keep(config)
+        logger.info(model.important_features)
         synthetic_data = generate_synthetic_data(n_samples=30)
         processed_synthetic_data = process_data(config, synthetic_data)
-        test_data = processed_synthetic_data[important_features]
-        if set(important_features) != set(test_data.columns):
+        test_data = processed_synthetic_data[model.important_features]
+        if set(model.important_features) != set(test_data.columns):
             raise ValueError(
                 "Mismatch in the number of features between"
                 "the model and the synthetic data"
@@ -62,7 +63,7 @@ async def startup_event():
             logger.info("All good!")
         test_data_as_dict = test_data.to_dict(orient="records")
         logger.info(test_data_as_dict)
-        predictions = make_prediction(test_data_as_dict)
+        predictions = model.make_prediction(test_data_as_dict)
         logger.info(predictions)
         logger.info(
             accuracy_score(processed_synthetic_data["Investment_Strategy"], predictions)
@@ -71,14 +72,9 @@ async def startup_event():
         logger.error(f"Error during startup: {e}")
 
 
-def make_prediction(data: List[Dict[str, Any]]) -> List:
-    data_df = pd.DataFrame(data)
-    return trained_model.predict(data_df).tolist()
-
-
 @app.post("/predict")
 async def predict(data: List[Dict[str, Any]]) -> List:
-    return make_prediction(data)
+    return model.make_prediction(data)
 
 
 # uvicorn fastapi_serving:app --reload
